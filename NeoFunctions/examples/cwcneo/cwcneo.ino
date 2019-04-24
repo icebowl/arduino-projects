5f7c1b9b571e7c104a1b301bb30adbc7c1fcfc28
icebowl <icebowl1967@gmail.com>
2019-04-24 05:19:09 -0700

    AA

---
+++ NeoFunctions/examples/cwcneo/cwcneo.ino
@@ -0,0 +1,34 @@
+// cw coleman  neo RGBW demo 1
+#include <NeoFunctions.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN 6
+#define NUM_LEDS 16
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+//set up variables
+int w = 0;
+int i; 
+
+void loop() {
+ for(i = 0; i < 16 ; i++){
+          //    strip.setPixelColor(pin number , strip.Color( red value,green value , blue value ,white value ) );
+          strip.setPixelColor(i, strip.Color(0,0,0,w ) );
+          
+ }
+ strip.show();
+ delay(30);
+  w++;
+  if  (w > 255) w = 0;
+}
